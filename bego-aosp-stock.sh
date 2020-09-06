a41f8a309b6ec945a7eb925832ba85334fc42cb3
ZyCromerZ <neetroid97@gmail.com>
2020-09-07 02:13:11 +0700

    bego-aosp-stock.sh: update branch and use non full lto build
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- bego-aosp-stock.sh
+++ bego-aosp-stock.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
-branch="20200724/root-upstream"
+branch="20200907/root-upstream"
 . main.sh 'initial'
 
+FullLto="Nope"
 spectrumFile="bego-on.rc"
 TypeBuild="STOCK"
 TypeBuildTag="AOSP"
