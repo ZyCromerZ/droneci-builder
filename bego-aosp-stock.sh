fd4b9837efcc13783bdc93585d4bcf1a04f9dfc5
ZyCromerZ <neetroid97@gmail.com>
2020-09-09 23:48:40 +0700

    go back build stock kernel
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- bego-aosp-stock.sh
+++ bego-aosp-stock.sh
@@ -1,19 +1,10 @@
 #! /bin/bash
-branch="20200907/main"
-. main-a.sh 'initial'
+branch="20200907/root-upstream"
+. main.sh 'initial'
 
 spectrumFile="bego-on.rc"
-TypeBuild="clang-9.0.8"
+TypeBuild="STOCK"
 TypeBuildTag="AOSP-CFW"
-getInfo ">> Building kernel . . . . <<"
-
-CompileKernel
-
-rm -rf $clangDir
-rm -rf $kernelDir/out
-git clone https://github.com/ZyCromerZ/google-clang -b 11.0.2-r383902b $clangDir --depth=1
-
-TypeBuild="clang-11.0.2"
 getInfo ">> Building kernel . . . . <<"
 
 CompileKernel
