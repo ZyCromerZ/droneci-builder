554bfd5a9ac27d60b82001d95181c927c8b34bc8
ZyCromerZ <neetroid97@gmail.com>
2020-10-11 08:35:03 +0700

    bego-aosp-private-c: update clang name
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- bego-aosp-private-c.sh
+++ bego-aosp-private-c.sh
@@ -6,6 +6,6 @@ spectrumFile="bego.rc"
 TypeBuild="Stable-Private"
 TypeBuildTag="AOSP-CFW"
 getInfo ">> Building kernel . . . . <<"
-KBUILD_COMPILER_STRING="Liyue Clang 9.0.4"
+KBUILD_COMPILER_STRING="Mondstadt Clang 9.0.4"
 
 CompileKernel
