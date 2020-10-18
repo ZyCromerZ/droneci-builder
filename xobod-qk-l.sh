d84ce22b4a93ba986ecdf80f97ee879e99c0de11
ZyCromerZ <neetroid97@gmail.com>
2020-10-18 23:06:10 +0700

    x01bd: add builder for qk l and ul
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

---
+++ xobod-qk-l.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+branch="20200911/qk-l"
+. main-b.sh 'initial'
+
+spectrumFile="none"
+TypeBuild="Stable-TEST"
+TypeBuildTag="AOSP"
+getInfo ">> Building kernel . . . . <<"
+
+CompileKernel
+CompileKernel "61"
+CompileKernel "62"
+CompileKernel "63"
+CompileKernel "64"
+CompileKernel "65"
+CompileKernel "66"
+CompileKernel "67"
+CompileKernel "68"
+CompileKernel "69"
+CompileKernel "70"
+CompileKernel "71"
+CompileKernel "72"
+
+FixPieWifi
+
+CompileKernel
+CompileKernel "61"
+CompileKernel "62"
+CompileKernel "63"
+CompileKernel "64"
+CompileKernel "65"
+CompileKernel "66"
+CompileKernel "67"
+CompileKernel "68"
+CompileKernel "69"
+CompileKernel "70"
+CompileKernel "71"
+CompileKernel "72"
