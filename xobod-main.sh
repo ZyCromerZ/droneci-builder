c72fd9de6342b9c1eb35958640010b1e2ff97fc1
ZyCromerZ <neetroid97@gmail.com>
2020-11-21 12:04:26 +0700

    x01bd: new rabase
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- xobod-main.sh
+++ xobod-main.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-branch="20200911/main"
+branch="20201121/main"
 . main-b.sh 'initial'
 
 spectrumFile="none"
@@ -8,17 +8,17 @@ TypeBuildTag="AOSP"
 getInfo ">> Building kernel . . . . <<"
 
 CompileKernel
-CompileKernel "65"
-CompileKernel "68"
-CompileKernel "71"
-CompileKernel "72"
+# CompileKernel "65"
+# CompileKernel "68"
+# CompileKernel "71"
+# CompileKernel "72"
 
 FixPieWifi
 
 CompileKernel
-CompileKernel "65"
-CompileKernel "68"
-CompileKernel "71"
-CompileKernel "72"
+# CompileKernel "65"
+# CompileKernel "68"
+# CompileKernel "71"
+# CompileKernel "72"
 
 tg_send_info "All $GetKernelName already uploaded to Gdrive :D"
